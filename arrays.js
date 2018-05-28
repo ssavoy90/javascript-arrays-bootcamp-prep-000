8491a5cdab2caa75b5457fe0051e49d66df4445f
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-28 21:32:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  v
+  return 
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element)
