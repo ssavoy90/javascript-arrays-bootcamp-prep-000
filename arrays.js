df1e5922968876636546c7bfab383f89327f1914
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-28 22:42:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-function addElementToEndOfArry(array, element) {
+function addElementToEndOfArray(array, element) {
   return array.push
 }
 function accessElementInArray(array, index) {
