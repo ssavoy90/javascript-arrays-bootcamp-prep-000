523b4afc75434e01e6af196850e6e4cc5e17e4a1
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-28 22:02:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,4 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
- return array = ['foo', 1]
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return chocolateBars.unshift ('mily way')
-}  
-
+ return array = ['foo', '1']
